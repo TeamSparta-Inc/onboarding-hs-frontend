a411f1be1356aa92b18859b97accecb42a3c81ef
hysngg <hs.kim@teamsparta.co>
2024-02-28 16:29:09 +0900

    feat : access,refresh token 발행 서비스 추가

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@nestjs/common'
+import { UsersService } from 'src/users/users.service'
+import { JwtService } from '@nestjs/jwt'
+import { SignUpDto } from './dto/signUp.dto';
+import { User } from 'src/users/schemas/user.schemas';
+
+
+@Injectable()
+export class AuthService {
+    constructor(
+      private usersService: UsersService,
+      private jwtService: JwtService
+    ) {}
+
+    async signUp(signUpDto : SignUpDto) : Promise<{user : User}>{
+        return
+    }
+
+    async signIn(){
+        return
+    }
+
+    async logOut(){}
+
+    async register(){}
+
+    async createAccessToken( username : Pick<SignUpDto, 'username'>){
+        const payload = { username }
+        return {
+            accessToken: this.jwtService.sign(payload)
+        }
+    }
+
+    async createRefreshToken(username : Pick<SignUpDto, 'username'>){
+        const payload = { username }
+        return { refreshToken : this.jwtService.sign(payload, { expiresIn : '600s'})}
+    }
+
+    async refreshAccessToken(refreshToken: string):Promise<{ accessToken : string}>{
+        const decodedRefreshToken = this.jwtService.verify(refreshToken)
+
+        const username = decodedRefreshToken.username
+        const user = await this.usersService.findOne(username)
+
+        const { accessToken } = await this.createAccessToken({username: user.username})
+        return { accessToken }
+    }
+}
