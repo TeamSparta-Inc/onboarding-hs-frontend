51c799fd2df95b4a660547a1f0a8132ab0de736f
hysngg <hs.kim@teamsparta.co>
2024-02-29 11:59:57 +0900

    fix : mongooseModule 연결 오류

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [ MongooseModule.forRoot('mongodb+srv://hskim:W7NX7xefVFHVDR9f@cluster0.7gmymj2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+  ),AuthModule,UsersModule ],
   controllers: [AppController],
   providers: [AppService],
 })
