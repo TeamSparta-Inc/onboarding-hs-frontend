c5dae72d583b757819facf0955b2d9d5acd5dae6
hysngg <hs.kim@teamsparta.co>
2024-02-29 12:07:27 +0900

    remove : 사용하지 않는 모듈 제거

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common'
 import { UsersService } from 'src/users/users.service'
 import { JwtService } from '@nestjs/jwt'
 import { SignUpDto } from './dto/signUp.dto';
-import { User } from 'src/users/schemas/user.schemas';
 import { SignInDto } from './dto/signin.dto';
 
 
