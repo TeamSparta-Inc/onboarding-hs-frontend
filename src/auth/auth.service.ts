c73e5867b28628786f8cde366668c7be22fd3738
hysngg <hs.kim@teamsparta.co>
2024-02-29 18:04:02 +0900

    chore : tab 사이즈 조정

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,59 +1,68 @@
-import { Injectable } from '@nestjs/common'
-import { UsersService } from 'src/users/users.service'
-import { JwtService } from '@nestjs/jwt'
+import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 import { SignUpDto } from './dto/signUp.dto';
 import { SignInDto } from './dto/signin.dto';
 
-
 @Injectable()
 export class AuthService {
-    constructor(
-      private usersService: UsersService,
-      private jwtService: JwtService
-    ) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
-    async signUp(signUpDto : SignUpDto) : Promise<{username : string}>{
+  async signUp(signUpDto: SignUpDto): Promise<{ username: string }> {
+    const { username, password } = signUpDto;
+    const { refreshToken } = await this.createRefreshToken({ username });
+    const user = await this.usersService.create({
+      username,
+      password,
+      refreshToken,
+    });
 
-        const { username, password } = signUpDto
-        const { refreshToken } = await this.createRefreshToken({ username })
-        const user = await this.usersService.create({ username, password, refreshToken})
+    return { username: user.username };
+  }
 
-        return { username : user.username }
-    }
+  async signIn(
+    signInDto: SignInDto,
+  ): Promise<{ accessToken: string; refreshToken: string }> {
+    const { username } = signInDto;
 
-    async signIn( signInDto : SignInDto ) : Promise<{ accessToken: string; refreshToken: string }>{
+    const user = this.usersService.findOne(username);
 
-        const { username } = signInDto
+    if (!user) throw new Error('존재하지 않는 유저입니다');
 
-        const user = this.usersService.findOne(username)
+    const { accessToken } = await this.createAccessToken({ username });
+    const { refreshToken } = await this.createRefreshToken({ username });
 
-        if(!user) throw new Error('존재하지 않는 유저입니다')
+    return { accessToken, refreshToken };
+  }
 
-        const { accessToken } = await this.createAccessToken({username})
-        const { refreshToken } = await this.createRefreshToken({username})
+  async createAccessToken(username: Pick<SignUpDto, 'username'>) {
+    const payload = { username };
+    return {
+      accessToken: this.jwtService.sign(payload),
+    };
+  }
 
-        return { accessToken, refreshToken }
-    }
+  async createRefreshToken(username: Pick<SignUpDto, 'username'>) {
+    const payload = { username };
+    return {
+      refreshToken: this.jwtService.sign(payload, { expiresIn: '600s' }),
+    };
+  }
 
-    async createAccessToken( username : Pick<SignUpDto, 'username'>){
-        const payload = { username }
-        return {
-            accessToken: this.jwtService.sign(payload)
-        }
-    }
+  async refreshAccessToken(
+    refreshToken: string,
+  ): Promise<{ accessToken: string }> {
+    const decodedRefreshToken = this.jwtService.verify(refreshToken);
 
-    async createRefreshToken(username : Pick<SignUpDto, 'username'>){
-        const payload = { username }
-        return { refreshToken : this.jwtService.sign(payload, { expiresIn : '600s'})}
-    }
+    const username = decodedRefreshToken.username;
+    const user = await this.usersService.findOne(username);
 
-    async refreshAccessToken(refreshToken: string):Promise<{ accessToken : string}>{
-        const decodedRefreshToken = this.jwtService.verify(refreshToken)
-
-        const username = decodedRefreshToken.username
-        const user = await this.usersService.findOne(username)
-
-        const { accessToken } = await this.createAccessToken({username: user.username})
-        return { accessToken }
-    }
+    const { accessToken } = await this.createAccessToken({
+      username: user.username,
+    });
+    return { accessToken };
+  }
 }
