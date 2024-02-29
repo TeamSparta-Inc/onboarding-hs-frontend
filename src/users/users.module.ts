5d5edf03f9fcf4d340a15137f783fa41c294de37
hysngg <hs.kim@teamsparta.co>
2024-02-29 12:02:10 +0900

    feat : 유저 모듈 추가

---
+++ src/users/users.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserModel } from './schemas/user.schemas';
+
+
+@Module({
+    imports: [
+        MongooseModule.forFeature([{name: User.name, schema : UserModel}])
+    ],
+    providers: [UsersService],
+    exports: [UsersService],
+})
+export class UsersModule {}
