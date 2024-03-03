fb1019d91b6a4d5e8b2b73db4761c613b6041018
hysngg <hs.kim@teamsparta.co>
2024-03-03 21:52:51 +0900

    feat : mongodb uri 환경변수화

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,21 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [ MongooseModule.forRoot('mongodb+srv://hskim:W7NX7xefVFHVDR9f@cluster0.7gmymj2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-  ),AuthModule,UsersModule ],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
