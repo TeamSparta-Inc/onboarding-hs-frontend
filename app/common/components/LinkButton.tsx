5ca098ce72715aadf3af82f74c50608bf3ccff47
hyesung99 <4538asd@naver.com>
2024-03-07 18:10:41 +0900

    fix : 실험 키 수정

--- app/common/components/LinkButton.tsx
+++ app/common/components/LinkButton.tsx
@@ -9,7 +9,7 @@ interface LinkButtonProps {
 export default function LinkButton({ href, text }: LinkButtonProps) {
   return (
     <>
-      <HackleExperiment experimentKey={247}>
+      <HackleExperiment experimentKey={266}>
         <HackleVariation variation={"A"}>
           <Link className="h-14 w-60" href={href}>
             <div className="flex justify-center items-center bg-red-500 hover:bg-red-600 text-white font-bold h-full w-full rounded">
@@ -18,7 +18,7 @@ export default function LinkButton({ href, text }: LinkButtonProps) {
           </Link>
         </HackleVariation>
       </HackleExperiment>
-      <HackleExperiment experimentKey={247}>
+      <HackleExperiment experimentKey={266}>
         <HackleVariation variation={"B"}>
           <Link className="h-14 w-60" href={href}>
             <div className="flex justify-center items-center bg-blue-500 hover:bg-blue-600 text-white font-bold h-full w-full rounded">
